ba6b12023611db9c5eff3dbbd75cc0787b4c4063
Preet Kumar <113340889+preettulas@users.noreply.github.com>
2023-01-09 12:23:50 +0530

    Create 202104123-add-start-in-elections.js

---
+++ migrations/202104123-add-start-in-elections.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("Elections", "start", {
+      type: Sequelize.DataTypes.BOOLEAN,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn("Elections", "start");
+  },
+};
+Footer
+© 2023 GitHub, Inc.
